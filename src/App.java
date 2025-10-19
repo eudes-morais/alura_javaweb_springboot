e9cf26aa14f928a0fc099f37078c30628f757006
eudes-morais <eudes.morais@gmail.com>
2025-10-19 20:38:03 -0300

    Updating movie class

--- src/App.java
+++ src/App.java
@@ -1,5 +1,22 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+
+        movie.name = "The Matrix";
+        movie.releaseYear = 1999;
+        // movie.includedInPlan = true;
+        // movie.rating = 8.7;
+        // movie.totalRatings = 1500;
+        movie.durationInMinutes = 136;
+
+        movie.displayTechnicalDetails();
+
+        movie.addRating(10.0);
+        movie.addRating(8.5);
+        movie.addRating(9.5);
+
+        System.out.println("Soma das avaliações: " + movie.sumOfRating);
+        System.out.println("Total de avaliações: " + movie.totalRatings);
+        System.out.println("Média das avaliações: " + movie.getAverageRating());
     }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -2,6 +2,31 @@ public class Movie {
     String name;
     int releaseYear;
     boolean includedInPlan;
-    double rating;
+    double sumOfRating;
     int totalRatings;
+    int durationInMinutes;
+
+    void displayTechnicalDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Release Year: " + releaseYear);
+        // System.out.println("Included in Plan: " + includedInPlan);
+        // System.out.println("Rating: " + rating);
+        // System.out.println("Total Ratings: " + totalRatings);
+        System.out.println("Duration (minutes): " + durationInMinutes);
+    }
+
+    void addRating(double newRating) {
+        // double totalScore = this.sumOfRating * this.totalRatings;
+        this.sumOfRating += newRating;
+        this.totalRatings++;
+        // this.sumOfRating = totalScore / this.totalRatings;
+    }
+
+    double getAverageRating() {
+        if (totalRatings == 0) {
+            return 0;
+        }
+        
+        return sumOfRating / totalRatings;
+    }
 }
