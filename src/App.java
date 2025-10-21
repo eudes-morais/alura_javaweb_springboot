075c9209172e337985f55c5ccdd0da496420ebb8
Eudes Morais <eudes.morais@gmail.com>
2025-10-21 19:12:43 -0300

    Creating new class

--- src/App.java
+++ src/App.java
@@ -1,13 +1,18 @@
+import br.com.screenmatch.models.ContaBancaria;
+import br.com.screenmatch.models.IdadePessoa;
+import br.com.screenmatch.models.Movie;
+import br.com.screenmatch.models.Produto;
+
 public class App {
     public static void main(String[] args) {
         Movie movie = new Movie();
 
-        movie.name = "The Matrix";
-        movie.releaseYear = 1999;
+        movie.setName("The Matrix");
+        movie.setReleaseYear(1999);
         // movie.includedInPlan = true;
         // movie.rating = 8.7;
         // movie.totalRatings = 1500;
-        movie.durationInMinutes = 136;
+        movie.setDurationInMinutes(136);
 
         movie.displayTechnicalDetails();
 
@@ -15,8 +20,51 @@ public class App {
         movie.addRating(8.5);
         movie.addRating(9.5);
 
-        System.out.println("Soma das avaliações: " + movie.sumOfRating);
-        System.out.println("Total de avaliações: " + movie.totalRatings);
+        System.out.println("Total das avaliações: " + movie.getTotalOfRating());
+        // System.out.println("Total de avaliações: " + movie.totalRatings);
         System.out.println("Média das avaliações: " + movie.getAverageRating());
+
+        // =========================================================================================
+
+        ContaBancaria conta = new ContaBancaria();
+        
+        conta.setNumeroConta(123);
+        conta.setSaldo(1000);
+        conta.titular = "João";
+
+        System.out.println("Número da Conta: " + conta.getNumeroConta());
+        System.out.println("Saldo: " + conta.getSaldo());
+        System.out.println("Titular: " + conta.titular);
+
+        conta.setSaldo(1500);
+        System.out.println("Novo Saldo: " + conta.getSaldo());
+
+        // =========================================================================================
+
+        IdadePessoa pessoa1 = new IdadePessoa();
+        pessoa1.setNome("Carowl");
+        pessoa1.setIdade(22);
+
+        IdadePessoa pessoa2 = new IdadePessoa();
+        pessoa2.setNome("Camila");
+        pessoa2.setIdade(20);
+
+        System.out.println(pessoa1.getNome() + " tem " + pessoa1.getIdade() + " anos.");
+        pessoa1.verificarIdade();
+
+        System.out.println(pessoa2.getNome() + " tem " + pessoa2.getIdade() + " anos.");
+        pessoa2.verificarIdade();
+
+        // =========================================================================================
+
+        Produto produto = new Produto();
+        produto.setNome("Celular");
+        produto.setPreco(2000.0);
+
+        System.out.println("Nome do Produto: " + produto.getNome());
+        System.out.println("Preço: " + produto.getPreco());
+
+        produto.aplicarDesconto(10);
+        System.out.println("Novo Preço após Desconto: " + produto.getPreco());
     }
 }
---
+++ src/br/com/screenmatch/models/IdadePessoa.java
@@ -0,0 +1,30 @@
+package br.com.screenmatch.models;
+
+public class IdadePessoa {
+    private int idade;
+    private String nome;
+
+    public void verificarIdade() {
+        if (idade >= 18) {
+            System.out.println(nome + " é maior de idade.");
+        } else {
+            System.out.println(nome + " é menor de idade.");
+        }
+    }
+
+    public int getIdade() {
+        return idade;
+    }  
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
