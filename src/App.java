5701cbba42553ff761eedbb16759c21d30eeb16e
eudes-morais <eudes.morais@gmail.com>
2025-10-23 10:52:28 -0300

    Updating files

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+import br.com.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.screenmatch.models.Movie;
 import br.com.screenmatch.models.Serie;
 
@@ -32,47 +33,15 @@ public class App {
         serie.setEpisodesPerSeason(12);
         System.out.println("Tempo da série: " + serie.getDurationInMinutes());
 
-        // =========================================================================================
+        Movie anotherMovie = new Movie();
+        anotherMovie.setName("Interstellar");
+        anotherMovie.setReleaseYear(2014);
+        anotherMovie.setDurationInMinutes(169);
 
-        // ContaBancaria conta = new ContaBancaria();
-        
-        // conta.setNumeroConta(123);
-        // conta.setSaldo(1000);
-        // conta.titular = "João";
-
-        // System.out.println("Número da Conta: " + conta.getNumeroConta());
-        // System.out.println("Saldo: " + conta.getSaldo());
-        // System.out.println("Titular: " + conta.titular);
-
-        // conta.setSaldo(1500);
-        // System.out.println("Novo Saldo: " + conta.getSaldo());
-
-        // =========================================================================================
-
-        // IdadePessoa pessoa1 = new IdadePessoa();
-        // pessoa1.setNome("Carowl");
-        // pessoa1.setIdade(22);
-
-        // IdadePessoa pessoa2 = new IdadePessoa();
-        // pessoa2.setNome("Camila");
-        // pessoa2.setIdade(20);
-
-        // System.out.println(pessoa1.getNome() + " tem " + pessoa1.getIdade() + " anos.");
-        // pessoa1.verificarIdade();
-
-        // System.out.println(pessoa2.getNome() + " tem " + pessoa2.getIdade() + " anos.");
-        // pessoa2.verificarIdade();
-
-        // =========================================================================================
-
-        // Produto produto = new Produto();
-        // produto.setNome("Celular");
-        // produto.setPreco(2000.0);
-
-        // System.out.println("Nome do Produto: " + produto.getNome());
-        // System.out.println("Preço: " + produto.getPreco());
-
-        // produto.aplicarDesconto(10);
-        // System.out.println("Novo Preço após Desconto: " + produto.getPreco());
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(movie);
+        calculadora.inclui(serie);
+        calculadora.inclui(anotherMovie);
+        System.out.println("Tempo total de maratona: " + calculadora.getTempoTotal());
     }
 }
